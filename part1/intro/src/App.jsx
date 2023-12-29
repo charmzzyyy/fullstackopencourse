0a893cf1d1bc257d59d0e021b32987f7352caf71
Charmaine Lee <>
2023-12-29 14:36:43 +0800

    Exercises 1.1.-1.2.

---
+++ part1/intro/src/App.jsx
@@ -0,0 +1,31 @@
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const friends = [ 'Peter', 'Maya']
+
+  return (
+    <div>
+      <p>{friends[0]} and {friends[1]}</p>
+    </div>
+  )
+}
+
+export default App
